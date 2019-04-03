426c941cab717b6ecfa0163fe4b8babaa222c994
josieyao <yao.josephine@yahoo.com>
2019-04-03 15:10:33 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", {
+      method: "POST",
+      body: {
+        "firstName": "Jo",
+        "registryMessage": "Congrats!"
+      }
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (content) {
+      return content.message
+    })
+}
+
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", {
+      method: "POST",
+      body: {
+        "firstName": "Jo",
+        "registryMessage": "Congrats!"
+      }
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (content) {
+      return content.message
+    })
+    .catch(function (error) {
+      alert("Error");
+      return ":(";
+    })
+}
