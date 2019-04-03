6e79822022c4c35688afd49471433b67594d41b7
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-04-03 15:09:30 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function registerSelf(){
+  return fetch('http://guestbook.example.com/register',{
+    'method':'POST',
+    'body': {
+    'firstName':'Jessica', 
+    'registryMessage':'You have been added to the guest book'}
+  })
+  .then(function(res){
+    return res.json();
+  }).then(function(stuff) {
+    return stuff.message})
+}
+
+function errorSelf(){
+  return fetch('http://guestbook.example.com/register-error',{
+    'method':'POST',
+    'body': {
+    'firstName':'Jessica', 
+    'registryMessage':'You have been added to the guest book'}
+  })
+  .then(function(res){
+    return res.json();
+  }).then(function(stuff) {
+
+    return stuff.message})
+    .catch(function(error){
+      alert("you've yeed yer last haw")
+      return ':('
+  
+    })
+}
