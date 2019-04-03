3f5381a9a1880dea5f4a2c0e7c97b9a1a1e26a76
github username <github email address>
2019-04-03 14:40:26 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let formData = JSON.stringify({
+  "firstName": "Mark",
+  "registryMessage": "Test registry message"
+})
+
+let configObj = {
+  method: "POST",
+  headers: { 'Content-Type': 'application/json' },
+  body: formData
+}
+
+function registerSelf() {
+  return fetch('http://guestbook.example.com/register', configObj)
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (returnedData) {
+      console.log(returnedData)
+      return returnedData.message
+    })
+}
+
+function errorSelf() {
+  return fetch('http://guestbook.example.com/register-error', configObj)
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (returnedData) {
+      console.log(returnedData)
+      return returnedData.message
+    })
+    .catch(function (error) {
+      return ":("
+    })
+}
