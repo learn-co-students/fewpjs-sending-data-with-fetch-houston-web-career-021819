08081a278ac24d67c74882e2ef5e621399fe2f66
EliLauff <eli.lauffenburger@gmail.com>
2019-04-03 15:38:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
+}
+
+function registerSelf(firstName, registryMessage) {
+  return fetch("http://guestbook.example.com/register", {
+    method: "POST",
+    body: {
+      firstName: firstName,
+      registryMessage: registryMessage
+    }
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      return json.message;
+    });
+}
+
+function errorSelf(firstName, registryMessage) {
+  return fetch("http://guestbook.example.com/register-error", {
+    method: "POST",
+    body: {
+      firstName: firstName,
+      registryMessage: registryMessage
+    }
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      return json.message;
+    })
+    .catch(function(error) {
+      alert("You've yee'd your last haw.");
+      return ":(";
+    });
 }
